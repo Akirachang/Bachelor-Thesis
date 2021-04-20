ea0dde2c90b244901ac8c7918ffeb452c1e87c7f
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-20 14:41:26 +0800

    60

--- getAlpha_angle/calculate_Alpha.py
+++ getAlpha_angle/calculate_Alpha.py
@@ -84,7 +84,7 @@ except Exception as e:
 
 finally:
     #dumps into json file
-    with open("json/50cm.json", "w") as write_file:
+    with open("json/60cm.json", "w") as write_file:
         json.dump(angle_dict, write_file)
 
     pipeline.stop()
