82daae0a09ce21487c582512d291907f5da148b5
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-21 17:13:25 +0800

    hi

--- Tests/settingTest/2.py
+++ Tests/settingTest/2.py
@@ -28,7 +28,7 @@ else:
 #***OPTION SETTING***
 profile = pipeline.start(config)
 s = profile.get_device().query_sensors()[1]
-s = s.set_option(rs.option.gain, 248)
+s = s.set_option(rs.option.gain, 200)
 #***OPTION SETTING***
 
 align_to = rs.stream.depth
