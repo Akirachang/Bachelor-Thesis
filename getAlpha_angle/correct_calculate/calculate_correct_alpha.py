2812529a4ef2eacf201aaa8a3907bf01b03bd9c4
常晟 <changs17@mails.tsinghua.edu.cn>
2021-04-23 12:59:28 +0800

    rearrange

--- getAlpha_angle/correct_calculate/calculate_correct_alpha.py
+++ getAlpha_angle/correct_calculate/calculate_correct_alpha.py
@@ -102,7 +102,7 @@ except Exception as e:
     pass
 
 finally:
-    with open("json/20cm.json", "w") as write_file:
+    with open("json/30cm.json", "w") as write_file:
         json.dump(angle_dict, write_file)
 
     pipeline.stop()
